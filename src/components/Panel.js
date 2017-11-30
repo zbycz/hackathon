ee5726dd6e55c51d9738ae251668985873b10d96
Pavel Zbytovsky <pavel.zbytovsky@gooddata.com>
2017-11-30 15:23:07 +0100

    hackathon final version

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,42 +1,94 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchOsmElement } from '../store/actions';
+import osmtogeojson from 'osmtogeojson';
+import leaflet from 'leaflet';
+import {get} from 'lodash';
 
+// https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3
 
 class Panel extends Component {
     constructor() {
         super();
+
+        this.submitX = this.submit.bind(this);
+
+        this.state = {
+            element: {},
+        };
     }
 
     componentDidMount() {
-        this.props.fetchOsmElement('n1601837931');
+        //this.fetchOsmElement('n1601837931');
+        //this.fetchOsmElement('n1601566699'); brno
+
+    }
+
+    submit() {
+        this.fetchOsmElement(this.inp.value);
     }
 
     render() {
-        if (this.props.hasErrored) {
-            return <p>Sorry! There was an error loading the items</p>;
+        if (this.state.error) {
+            return <p>Sorry! There was an error loading the items {this.state.e}</p>;
         }
-        if (this.props.isLoading) {
+        if (this.state.isLoading) {
             return <p>Loading…</p>;
         }
 
         return (
-            <div className="App">
+            <div id="panel" className="App">
+                id: <input type="text" ref={(inp)=>this.inp=inp} onChange={this.submitX} />
+
+                <h2>{get(this, 'state.element.properties.name', 'no-name')}</h2>
+
+                Geojson:
+                <pre>
+                {JSON.stringify(this.state.element, null, ' ')}
+                </pre>
             </div>
         );
     }
+
+
+    fetchOsmElement(id) {
+        const type = { w: 'way', n: 'node', r: 'relation' }[id[0]];
+        const idx = id.substr(1);
+        const url = `https://www.openstreetmap.org/api/0.6/${type}/${idx}`;
+
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                this.setState({ isLoading: false });
+                return response;
+            })
+            .then((response) => response.text())
+            .then(xmlStr => {
+                const xmlDom = (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
+                return osmtogeojson(xmlDom);
+            })
+            .then((element) => {
+                const feature = element.features[0];
+
+                var lon = feature.geometry.coordinates[0];
+                var lat = feature.geometry.coordinates[1];
+                this.props.map().setView([lat, lon]);
+                leaflet.marker([lat, lon]).addTo(this.props.map());
+
+
+                console.log("inp", this.inp);
+                this.inp.value = feature.id;
+
+                this.setState({ element: feature })
+            })
+            .catch((e) => {
+                console.log("er", e);
+                this.setState({ error: e })
+            });
+    }
+
 }
 
-const mapStateToProps = (state) => {
-    return {
-        items: state.items,
-        hasErrored: state.itemsHasErrored,
-        isLoading: state.itemsIsLoading
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchOsmElement: (id) => dispatch(fetchOsmElement(id))
-    };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Panel);
+
+export default Panel;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/index.scss';
+import './assets/index.css';
 
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { store } from './store/index';
 import Panel from './components/Panel'
+import leaflet from 'leaflet';
+
+
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Panel className={"panel"} />
+    <div>
+        <Panel map={()=>mymap} />
         <div id="map" />
-    </Provider>,
+    </div>,
     document.getElementById('root'));
+
+
+var mymap = leaflet.map('map').setView([49.1945, 16.6113], 15);
+// leaflet.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+//     attribution: '&copy; CC-BY-SA <a href="https://openstreetmap.org/copyright">OSM</a>, imagery <a href="http://maps.stamen.com">Stamen Design</a>',
+//     maxZoom: 18,
+//     code: 's'
+// }).addTo(mymap);
+
+var mapboxAPIkey = "pk.eyJ1IjoiemJ5Y3oiLCJhIjoiY2owa3hrYjF3MDAwejMzbGM4aDNybnhtdyJ9.8CIw6X6Jvmk2GwCE8Zx8SA";
+var mapbox = leaflet.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=' + mapboxAPIkey, {
+    maxZoom: 24,
+    attribution: " <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>",
+    code: 'x',
+    osmczDefaultLayer: true,
+    basic: true
+}).addTo(mymap);
+
+
+
 
 registerServiceWorker();
 
